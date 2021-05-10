72764ae04546cbb74bf078dc3869bf1892f2cf07
Finn Voorhees <finnvoorhees@gmail.com>
2021-05-10 11:26:50 +0100

    Initial Commit

---
+++ DrawAR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DrawAR
+//
+//  Created by Finn Voorhees on 10/05/2021.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
